cd165bcb7691a0ebc5ef48656612c952d001af8f
Dano Oravec <danoravec@gmail.com>
2017-03-19 19:28:24 +0100

    Example

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 // Inspired by "The Coding Train"
 int main() {
-    Init evolution(1000, "unicorn");
+    int max_population = 1000;
+    string target = "The sky is dark!";
+    Init evolution(max_population, target);
     evolution.evolve();
 
     return 0;
